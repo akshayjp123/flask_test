696fd8ef453485871673533092818586d6476a17
akshay <akshayjpatil11@gmail.com>
2019-06-18 11:21:14 -0400

    testing

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ api = Api(app)
 
 class HelloWorld(Resource):
     def get(self):
-        return {'hhello': 'world','new hello':'new world!','another':'not again'}
+        return {'hello': 'world','new hello':'new world!','another':'not again'}
 
 api.add_resource(HelloWorld, '/')
 
